7bed29d98e28ea1023bd4d703d234e830b1cc78f
Idzhans Khairi <muhammad.idzhans@enfrasys.com>
2025-09-20 02:40:54 +0800

    Add new things

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit  } from '@angular/core';
+
+declare var bootstrap: any; // Access Bootstrap JS
 
 @Component({
   selector: 'app-home',
@@ -7,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrl: './home.scss'
 })
 export class Home {
+
+    ngAfterViewInit() {
+    const tabElements = document.querySelectorAll('.nav-link[data-bs-toggle="tab"]');
+    const collapseElement = document.getElementById('aboutMore');
+
+    tabElements.forEach(tab => {
+      tab.addEventListener('shown.bs.tab', () => {
+        if (collapseElement && collapseElement.classList.contains('show')) {
+          const bsCollapse = bootstrap.Collapse.getInstance(collapseElement);
+          if (bsCollapse) {
+            bsCollapse.hide();
+          }
+        }
+      });
+    });
+  }
 
 }
