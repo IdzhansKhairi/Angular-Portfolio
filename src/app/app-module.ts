22228fb1c5193707b4ef11e55ce93e450ec65971
Idzhans Khairi <muhammad.idzhans@enfrasys.com>
2025-09-20 01:53:21 +0800

    Initial commit

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,16 +1,33 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 
+// Components for routes import
+import { Home } from './pages/home/home';
+import { Contact } from './pages/contact/contact';
+import { Sidebar } from './layout/sidebar/sidebar';
+import { Resume } from './pages/resume/resume';
+
 @NgModule({
   declarations: [
-    App
+    App,
+    Home,
+    Contact,
+    Sidebar,
+    Resume
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
---
+++ src/app/pages/contact/contact.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-contact',
+  standalone: false,
+  templateUrl: './contact.html',
+  styleUrl: './contact.scss'
+})
+export class Contact {
+  
+}
